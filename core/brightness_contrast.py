93d9acd3e3441e0d03ffe4ad82c527f91c6fbf04
Anubhav Jindal <152766628+Anubhav1406@users.noreply.github.com>
2026-03-30 17:32:21 +0530

    modified brightness and contrast logic

--- core/brightness_contrast.py
+++ core/brightness_contrast.py
@@ -1,7 +1,12 @@
 import cv2
+import numpy as np
 
 def apply_brightness(img, b):
-    return cv2.convertScaleAbs(img, alpha=1, beta=b)
+    img_inter = img.astype(np.float32) + b
+    np.clip(img_inter, 0, 255, out=img_inter)
+    return img_inter.astype(np.uint8)
 
 def apply_contrast(img, a):
-    return cv2.convertScaleAbs(img, alpha=a, beta=0)
+    img_inter = a*(img.astype(np.float32)-128)+128
+    np.clip(img_inter, 0, 255, out=img_inter)
+    return img_inter.astype(np.uint8)
